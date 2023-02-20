a909d64b04329290aa7bf78b7f2506cfbe6b73b5
Georgvit <ifreyd@mail.ru>
2023-02-20 23:10:55 +0300

    чтение и запись

--- src/controllers/NotesController.py
+++ src/controllers/NotesController.py
@@ -5,12 +5,36 @@ class NotesController:
     def createNote(self, notes):
         self.test = {"id": notes.count, 'date': notes.date, 'topic': notes.topic, 'text': notes.text}
         self.noteslist.append(self.test)
+        self.encrypted(self.test)
 
     def printNote(self):
-        for s in range(len(self.noteslist)):
-            self.printTest(self.noteslist[s])
+        self.readText()
+        # for s in range(len(self.noteslist)):
+        #     self.printTest(self.noteslist[s])
+
 
 
     def printTest(self,test):
         print(f"id: {test.get('id')} \nДата создания: {test.get('date')}\n"
-              f"Тема: {test.get('topic')}\nТекст: {test.get('text')}")
+              f"Тема: {test.get('topic')}\nТекст: {test.get('text')}\n")
+
+
+    def encrypted(self,test):
+        text = (f"id:{test.get('id')};date:{test.get('date')};"
+              f"topic:{test.get('topic')};text:{test.get('text')}\n")
+        # Создаем и открываем файлы для записи оригинального текста
+        o_text = open('original_text.txt', 'a+')
+
+        # Записываем данные в файл и закрываем их
+        o_text.write(text)
+        o_text.close()
+
+    def readText(self):
+        with open('original_text.txt', 'r') as file:
+            for line in file.readlines():
+                listtest = list(line.split(";"))
+                for s in listtest:
+                    print(s)
+
+
+
