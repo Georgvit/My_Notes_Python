6c2ec7b4df6c5c66b17a6498f06d3461730af242
GeorgDeveloper <ifreyd@yandex.ru>
2023-02-25 12:00:54 +0300

    Логирование

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import  logging
 from view.NotesView import NotesView
 
-note = NotesView()
-note.run()
+logging.basicConfig(level=logging.ERROR, filename='py_log.log', filemode='w')
+try:
+    note = NotesView()
+    note.run()
+except:
+    logging.error('Error startig')
+
 
